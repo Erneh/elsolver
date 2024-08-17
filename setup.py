f339e3491fab2e4bfd0e3879bb92f2fa1bcf1b9e
ErnestPerez <ernestpech11@gmail.com>
2024-08-17 02:24:32 +0200

    setup

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      
-    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.11',
     'Programming Language :: Python :: 3.7',
